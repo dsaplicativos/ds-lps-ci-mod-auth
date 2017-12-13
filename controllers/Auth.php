789e816157fc86a4544d9e07a72fe9d45ed6fcb7
Unknown <stobenetti@gmail.com>
2017-12-13 17:04:26 -0200

    Criação de variável para adaptação ao HMVC

--- controllers/Auth.php
+++ controllers/Auth.php
@@ -5,8 +5,10 @@
  * @property Ion_auth|Ion_auth_model $ion_auth        The ION Auth spark
  * @property CI_Form_validation      $form_validation The form validation library
  */
-class Auth extends CI_Controller
+class Auth extends MY_Controller
 {
+
+    private $data;
 
 	public function __construct()
 	{
